db238eaec65d65e240f2d81369a3c4badd36a2d6
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-03-26 18:41:06 +0300

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { CirclesWithBar } from 'react-loader-spinner';
 import { getImages } from 'services/getImages';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -6,45 +6,43 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import { Button } from './Button/Button';
 
-export class App extends Component {
-  state = {
-    response: [],
-    page: 1,
-    inputValue: '',
-    loading: false,
-  };
+export function App() {
+  const [response, setResponse] = useState([]);
+  const [page, setPage] = useState(1);
+  const [inputValue, setInputValue] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  onLoadResults = async () => {
-    this.setState({ loading: true });
-    const promise = await getImages(this.state.inputValue, this.state.page);
-    getImages(this.state.inputValue, this.state.page).finally(() =>
-      this.setState({ loading: false })
+  const onLoadResults = async page => {
+    setLoading(true);
+
+    const promise = await getImages(inputValue, page).finally(() =>
+      setLoading(false)
     );
-    this.setState(prevState => {
-      return {
-        response: [...prevState.response, ...promise.hits],
-        page: prevState.page + 1,
-      };
-    });
+
+    if (page === 1) {
+      setResponse([...promise.hits]);
+    } else {
+      setResponse(prevState => [...prevState, ...promise.hits]);
+    }
   };
 
-  onChange = e => {
-    this.setState({ inputValue: e.target.value });
+  const onChange = e => {
+    setInputValue(e.target.value);
   };
 
-  render() {
-    return (
-      <div>
-        <Searchbar
-          onSubmit={e => {
-            e.preventDefault();
-            this.setState({ response: [], page: 1 });
-            setTimeout(() => this.onLoadResults(), 100);
-          }}
-          onChange={this.onChange}
-        />
-        <ImageGallery>
-          {this.state.response.map(item => (
+  return (
+    <div>
+      <Searchbar
+        onSubmit={e => {
+          e.preventDefault();
+          onLoadResults(1);
+          setPage(1);
+        }}
+        onChange={onChange}
+      />
+      <ImageGallery>
+        {response &&
+          response.map(item => (
             <ImageGalleryItem
               key={item.id}
               id={item.id}
@@ -52,20 +50,88 @@ export class App extends Component {
               largeIMG={item.largeImageURL}
             />
           ))}
-        </ImageGallery>
+      </ImageGallery>
 
-        {this.state.loading && (
-          <div className="audio">
-            <div className="audio-margin">
-              <CirclesWithBar height="40" width="40" color="blue" />
-            </div>
+      {loading && (
+        <div className="audio">
+          <div className="audio-margin">
+            <CirclesWithBar height="40" width="40" color="blue" />
           </div>
-        )}
+        </div>
+      )}
 
-        {this.state.response.length > 0 && !this.state.loading && (
-          <Button onClick={this.onLoadResults} />
-        )}
-      </div>
-    );
-  }
+      {response.length > 0 && !loading && (
+        <Button
+          onClick={() => {
+            onLoadResults(page + 1);
+            setPage(page + 1);
+          }}
+        />
+      )}
+    </div>
+  );
 }
+
+// export class App extends Component {
+//   state = {
+//     response: [],
+//     page: 1,
+//     inputValue: '',
+//     loading: false,
+//   };
+
+// onLoadResults = async () => {
+//   this.setState({ loading: true });
+//   const promise = await getImages(this.state.inputValue, this.state.page);
+//   getImages(this.state.inputValue, this.state.page).finally(() =>
+//     this.setState({ loading: false })
+//   );
+//   this.setState(prevState => {
+//     return {
+//       response: [...prevState.response, ...promise.hits],
+//       page: prevState.page + 1,
+//     };
+//   });
+// };
+
+// onChange = e => {
+//   this.setState({ inputValue: e.target.value });
+// };
+
+//   render() {
+// return (
+//   <div>
+//     <Searchbar
+//       onSubmit={e => {
+//         e.preventDefault();
+//         this.setState({ response: [], page: 1 });
+//         setTimeout(() => this.onLoadResults(), 100);
+//       }}
+//       onChange={this.onChange}
+//     />
+//     <ImageGallery>
+//       {this.state.response.map(item => (
+//         <ImageGalleryItem
+//           key={item.id}
+//           id={item.id}
+//           src={item.webformatURL}
+//           largeIMG={item.largeImageURL}
+//         />
+//       ))}
+//     </ImageGallery>
+
+//     {this.state.loading && (
+//       <div className="audio">
+//         <div className="audio-margin">
+//           <CirclesWithBar height="40" width="40" color="blue" />
+//         </div>
+//       </div>
+//     )}
+
+//     {this.state.response.length > 0 && !this.state.loading && (
+//       <Button onClick={this.onLoadResults} />
+//     )}
+//   </div>
+// );
+//   }
+// }
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,44 +1,78 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Modal } from '../Modal/Modal';
 
-export class ImageGalleryItem extends Component {
-  state = { isModalOpen: false, largeURL: '' };
+export function ImageGalleryItem({ id, src, largeIMG }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [largeURL, setLargeURL] = useState('');
 
-  closeESC = () => {
-    this.setState({ isModalOpen: !true });
+  const closeESC = () => {
+    setIsModalOpen(!true);
   };
 
-  openModal = largeURL => {
-    this.setState({ isModalOpen: !false, largeURL });
+  const openModal = largeURL => {
+    setIsModalOpen(!false);
+    setLargeURL(largeURL);
   };
 
-  closeModal = e => {
+  const closeModal = e => {
     if (e.target.className === 'overlay') {
-      this.setState({ isModalOpen: !true });
+      setIsModalOpen(!true);
     }
   };
 
-  render() {
-    return (
-      <div>
-        <li
-          className="gallery-item"
-          key={this.props.id}
-          onClick={() => this.openModal(this.props.largeIMG)}
-        >
-          <img src={this.props.src} alt="" />
-        </li>
+  return (
+    <div>
+      <li className="gallery-item" key={id} onClick={() => openModal(largeIMG)}>
+        <img src={src} alt="" />
+      </li>
 
-        {this.state.isModalOpen && this.state.largeURL !== undefined && (
-          <Modal state={this.closeModal} close={this.closeESC}>
-            <img className="large-img" src={this.state.largeURL} alt="" />
-          </Modal>
-        )}
-      </div>
-    );
-  }
+      {isModalOpen && largeURL !== undefined && (
+        <Modal state={closeModal} close={closeESC}>
+          <img className="large-img" src={largeURL} alt="" />
+        </Modal>
+      )}
+    </div>
+  );
 }
+
+// export class ImageGalleryItem extends Component {
+//   state = { isModalOpen: false, largeURL: '' };
+
+// closeESC = () => {
+//   this.setState({ isModalOpen: !true });
+// };
+
+//   openModal = largeURL => {
+//     this.setState({ isModalOpen: !false, largeURL });
+//   };
+
+//   closeModal = e => {
+//     if (e.target.className === 'overlay') {
+//       this.setState({ isModalOpen: !true });
+//     }
+//   };
+
+//   render() {
+// return (
+//   <div>
+//     <li
+//       className="gallery-item"
+//       key={this.props.id}
+//       onClick={() => this.openModal(this.props.largeIMG)}
+//     >
+//       <img src={this.props.src} alt="" />
+//     </li>
+
+//     {this.state.isModalOpen && this.state.largeURL !== undefined && (
+//       <Modal state={this.closeModal} close={this.closeESC}>
+//         <img className="large-img" src={this.state.largeURL} alt="" />
+//       </Modal>
+//     )}
+//   </div>
+// );
+//   }
+// }
 
 ImageGalleryItem.propTypes = {
   id: PropTypes.number,
